91daceea832d8657b914b45ea1f6c2ede7959e29
zeeshansGitHub <zeeshan.abbas@hotmail.co.uk>
2023-09-10 23:46:50 +0100

    Update My-Test-NoteBook21.py

--- My-Test-NoteBook21.py
+++ My-Test-NoteBook21.py
@@ -1,7 +1,2 @@
 %md
-
-### Step 1: Set the data location and type
-
-There are two ways to access Azure Blob storage: account keys and shared access signatures (SAS).
-
-To get started, we need to set the location and type of the file.
+### Step 1:
